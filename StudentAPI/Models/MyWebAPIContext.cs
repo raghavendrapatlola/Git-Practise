dc2e15ac83daf90c80dca56a9662d64d2c7caa4a
raghavendrapatlola <raghavendrapatlola@gmail.com>
2019-12-26 21:34:56 -0500

    my first commit

---
+++ StudentAPI/Models/MyWebAPIContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace StudentAPI.Models
+{
+    public class MyWebAPIContext:DbContext
+    {
+        public MyWebAPIContext(DbContextOptions<MyWebAPIContext> options ):base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Group { get; set; }
+    }
+}
